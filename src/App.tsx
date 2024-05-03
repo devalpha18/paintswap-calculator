69974082cc65a0af8ef64f2da21287405066521e
sage0711 <pamewabnnx@gmail.com>
2024-05-03 14:19:54 -0400

    initial test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Calculator from "./Calculator";
+import Calculator from "./calculator";
 import { Container } from "@mui/material";
 
 const App: React.FC = () => {
